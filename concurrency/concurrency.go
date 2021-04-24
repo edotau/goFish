3df64dbadd46f53571275a8728227a0ed1e8d94d
edotau <ericdotau@gmail.com>
2021-04-24 09:10:40 -0700

    general concurrency concepts

--- concurrency/concurrency.go
+++ concurrency/concurrency.go
@@ -1,3 +1,71 @@
 // Package concurrency contains functions used to build concurrent processes and/or tasks in both pipelines and workflows
 // very much still in development
 package concurrency
+
+import (
+	"sync"
+)
+
+// Goroutine is a an alias of a function designed to be programed in concurrency
+type Goroutine func() error
+
+// Run calls the passed functions in a goroutine, returns a chan of errors.
+func RunConcur(functions ...Goroutine) chan error {
+	total := len(functions)
+	errs := make(chan error, total)
+
+	var wg sync.WaitGroup
+	wg.Add(total)
+
+	go func(errs chan error) {
+		wg.Wait()
+		close(errs)
+	}(errs)
+
+	for _, i := range functions {
+		go func(i Goroutine, errs chan error) {
+			defer wg.Done()
+			errs <- i()
+		}(i, errs)
+	}
+	return errs
+}
+
+func RaceCondition(concurrency int, tasks ...Goroutine) chan error {
+	total := len(tasks)
+
+	if concurrency <= 0 {
+		concurrency = 1
+	}
+
+	if concurrency > total {
+		concurrency = total
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(total)
+
+	errs := make(chan error, total)
+	go func(errs chan error) {
+		wg.Wait()
+		close(errs)
+	}(errs)
+
+	sem := make(chan struct{}, concurrency)
+	defer func(sem chan<- struct{}) { close(sem) }(sem)
+
+	for _, fn := range tasks {
+		go func(fn Goroutine, sem <-chan struct{}, errs chan error) {
+			defer wg.Done()
+			defer func(sem <-chan struct{}) { <-sem }(sem)
+
+			errs <- fn()
+		}(fn, sem, errs)
+	}
+
+	for i := 0; i < cap(sem); i++ {
+		sem <- struct{}{}
+	}
+
+	return errs
+}
---
+++ concurrency/concurrency_test.go
@@ -0,0 +1,133 @@
+package concurrency
+
+import (
+	"errors"
+	"testing"
+	"time"
+)
+
+var (
+	fn1     = func() error { return nil }
+	fn2     = func() error { return errors.New("BOOM!") }
+	timeout = time.After(2 * time.Second)
+)
+
+func TestConcurrency(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		scenario string
+		function func(*testing.T)
+	}{
+		{
+			scenario: "test run",
+			function: testRun,
+		},
+		{
+			scenario: "test run limit",
+			function: testaceCondition,
+		},
+		{
+			scenario: "test run limit with negative concurrency value",
+			function: testaceConditionWithNegativeConcurrencyValue,
+		},
+		{
+			scenario: "test run limit with concurrency value greater than passed functions",
+			function: testaceConditionWithConcurrencyGreaterThanPassedFunctions,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.scenario, func(t *testing.T) {
+			test.function(t)
+		})
+	}
+}
+
+func testRun(t *testing.T) {
+	var count int
+	err := RunConcur(fn1, fn2)
+outer:
+	for {
+		select {
+		case <-err:
+			count++
+			if count == 2 {
+				break outer
+			}
+		case <-timeout:
+			t.Errorf("parallel.Run() failed, got timeout error")
+			break outer
+		}
+	}
+
+	if count != 2 {
+		t.Errorf("parallel.Run() failed, got '%v', expected '%v'", count, 2)
+	}
+}
+
+func testaceCondition(t *testing.T) {
+	var count int
+	err := RaceCondition(2, fn1, fn2)
+outer:
+	for {
+		select {
+		case <-err:
+			count++
+			if count == 2 {
+				break outer
+			}
+		case <-timeout:
+			t.Errorf("parallel.Run() failed, got timeout error")
+			break outer
+		}
+	}
+
+	if count != 2 {
+		t.Errorf("parallel.Run() failed, got '%v', expected '%v'", count, 2)
+	}
+}
+
+func testaceConditionWithNegativeConcurrencyValue(t *testing.T) {
+	var count int
+	err := RaceCondition(-1, fn1, fn2)
+outer:
+	for {
+		select {
+		case <-err:
+			count++
+			if count == 2 {
+				break outer
+			}
+		case <-timeout:
+			t.Errorf("parallel.Run() failed, got timeout error")
+			break outer
+		}
+	}
+
+	if count != 2 {
+		t.Errorf("parallel.Run() failed, got '%v', expected '%v'", count, 2)
+	}
+}
+
+func testaceConditionWithConcurrencyGreaterThanPassedFunctions(t *testing.T) {
+	var count int
+	err := RaceCondition(3, fn1, fn2)
+outer:
+	for {
+		select {
+		case <-err:
+			count++
+			if count == 2 {
+				break outer
+			}
+		case <-timeout:
+			t.Errorf("parallel.Run() failed, got timeout error")
+			break outer
+		}
+	}
+
+	if count != 2 {
+		t.Errorf("parallel.Run() failed, got '%v', expected '%v'", count, 2)
+	}
+}
