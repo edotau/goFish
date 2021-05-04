e88952ece8092c1dccba8f2fb7b23379b4813791
edotau <ericdotau@gmail.com>
2021-05-04 02:07:50 -0700

    bam reader

--- bam/bai.go
+++ bam/bai.go
@@ -3,8 +3,13 @@
 package bam
 
 import (
+	"bufio"
+	"encoding/binary"
+	"fmt"
+	"io"
+	"sort"
+
 	"github.com/edotau/goFish/simpleio"
-	htslib "github.com/grailbio/bio/encoding/bam"
 	"github.com/grailbio/hts/bgzf"
 )
 
@@ -43,11 +48,13 @@ type Metadata struct {
 }
 
 // IndexReader decompress a binary index of r and returns an Index or nil and an error.
-func IndexReader(filename string) *htslib.Bai {
+func IndexReader(filename string) *Bai {
 	file := simpleio.NewReader(filename)
-	reader, err := htslib.IndexReader(file)
-	simpleio.ErrorHandle(err)
-	return reader
+	//index =
+	return IndexBai(file.Reader)
+	//simpleio.ErrorHandle(err)
+
+	//return index
 }
 
 func toOffset(voffset uint64) bgzf.Offset {
@@ -59,4 +66,159 @@ func toOffset(voffset uint64) bgzf.Offset {
 
 func fromOffset(offset bgzf.Offset) uint64 {
 	return uint64(offset.File<<16) | uint64(offset.Block)
+}
+
+// ReadIndex parses the content of r and returns an Index or nil and an error.
+func IndexBai(reader io.Reader) *Bai {
+	r := bufio.NewReaderSize(reader, 4<<20)
+	i := &Bai{}
+	var err error
+	if _, err = io.ReadFull(r, i.Magic[0:]); err != nil {
+		simpleio.ErrorHandle(err)
+	}
+	if i.Magic != [4]byte{'B', 'A', 'I', 0x1} {
+		simpleio.ErrorHandle(fmt.Errorf("bam index invalid magic: %v", i.Magic))
+	}
+
+	var refCount int32
+	if err = binary.Read(r, binary.LittleEndian, &refCount); err != nil {
+		simpleio.ErrorHandle(err)
+	}
+	i.Refs = make([]Reference, refCount)
+
+	// Read each Reference
+	for refId := 0; int32(refId) < refCount; refId++ {
+		// Read each Bin
+		var binCount int32
+		if err = binary.Read(r, binary.LittleEndian, &binCount); err != nil {
+			simpleio.ErrorHandle(err)
+		}
+		ref := Reference{
+			Bins: make([]bin, 0, binCount),
+		}
+		for b := 0; int32(b) < binCount; b++ {
+			var binNum uint32
+			if err = binary.Read(r, binary.LittleEndian, &binNum); err != nil {
+				simpleio.ErrorHandle(err)
+			}
+			var chunkCount int32
+			if err = binary.Read(r, binary.LittleEndian, &chunkCount); err != nil {
+				simpleio.ErrorHandle(err)
+			}
+
+			bin := bin{
+				binNum: binNum,
+				Chunks: make([]Chunk, chunkCount),
+			}
+
+			// Read each Chunk
+			bin.getNextChunk(r, chunkCount)
+
+			if binNum == 37450 {
+				// If we have a metadata chunk, put it in ref.Meta instead of ref.Bins.
+				if len(bin.Chunks) != 2 {
+					simpleio.ErrorHandle(fmt.Errorf("invalid metadata: chunk has %d chunks, should have 2", len(bin.Chunks)))
+				}
+				ref.Meta = Metadata{
+					UnmappedBegin: fromOffset(bin.Chunks[0].Begin),
+					UnmappedEnd:   fromOffset(bin.Chunks[0].End),
+					MappedCount:   fromOffset(bin.Chunks[1].Begin),
+					UnmappedCount: fromOffset(bin.Chunks[1].End),
+				}
+			} else {
+				ref.Bins = append(ref.Bins, bin)
+			}
+		}
+
+		// Read each Interval.
+		var intervalCount int32
+		if err = binary.Read(r, binary.LittleEndian, &intervalCount); err != nil {
+			simpleio.ErrorHandle(err)
+		}
+		ref.Intervals = make([]bgzf.Offset, intervalCount)
+		for inv := 0; int32(inv) < intervalCount; inv++ {
+			var ioffset uint64
+			if err = binary.Read(r, binary.LittleEndian, &ioffset); err != nil {
+				simpleio.ErrorHandle(err)
+			}
+			ref.Intervals[inv] = toOffset(ioffset)
+		}
+		i.Refs[refId] = ref
+	}
+
+	var unmappedCount uint64
+	if err = binary.Read(r, binary.LittleEndian, &unmappedCount); err == nil {
+		i.UnmappedCount = &unmappedCount
+	} else if err != nil && err != io.EOF {
+		simpleio.ErrorHandle(err)
+	}
+	return i
+}
+
+func (block bin) getNextChunk(reader io.Reader, chunkCount int32) {
+	var err error
+	// Read each Chunk
+	for c := 0; int32(c) < chunkCount; c++ {
+		var beginOffset uint64
+		if err = binary.Read(reader, binary.LittleEndian, &beginOffset); err != nil {
+			simpleio.ErrorHandle(err)
+		}
+		var endOffset uint64
+		if err = binary.Read(reader, binary.LittleEndian, &endOffset); err != nil {
+			simpleio.ErrorHandle(err)
+		}
+		block.Chunks[c] = Chunk{
+			Begin: toOffset(beginOffset),
+			End:   toOffset(endOffset),
+		}
+	}
+
+}
+
+// AllOffsets returns a map of chunk offsets in the index file, it
+// includes chunk begin locations, and interval locations.  The Key of
+// the map is the Reference ID, and the value is a slice of
+// bgzf.Offsets.  The return map will have an entry for every
+// reference ID, even if the list of offsets is empty.
+func (i *Bai) AllOffsets() map[int][]bgzf.Offset {
+	m := make(map[int][]bgzf.Offset)
+	for refId, ref := range i.Refs {
+		m[refId] = make([]bgzf.Offset, 0)
+
+		// Get the offsets for this ref.
+		for _, bin := range ref.Bins {
+			for _, chunk := range bin.Chunks {
+				if chunk.Begin.File != 0 || chunk.Begin.Block != 0 {
+					m[refId] = append(m[refId], chunk.Begin)
+				}
+			}
+		}
+		for _, interval := range ref.Intervals {
+			if interval.File != 0 || interval.Block != 0 {
+				m[refId] = append(m[refId], interval)
+			}
+		}
+
+		// Sort the offsets
+		sort.SliceStable(m[refId], func(i, j int) bool {
+			c0 := m[refId][i]
+			c1 := m[refId][j]
+			if c0.File != c1.File {
+				return c0.File < c1.File
+			}
+			return c0.Block < c1.Block
+		})
+
+		// Keep only unique offsets
+		uniq := make([]bgzf.Offset, 0)
+		previous := bgzf.Offset{-1, 0}
+		for _, offset := range m[refId] {
+			if offset != previous {
+				uniq = append(uniq, offset)
+				previous = offset
+			}
+		}
+		m[refId] = uniq
+	}
+	return m
 }
--- bam/bai_test.go
+++ bam/bai_test.go
@@ -1,92 +1,10 @@
 package bam
 
 import (
-	"bytes"
-	"encoding/binary"
-	"fmt"
-	"io"
-	"strconv"
-	"strings"
 	"testing"
-
-	"github.com/edotau/goFish/simpleio"
-	"github.com/grailbio/testutil/expect"
-	"github.com/stretchr/testify/assert"
 )
 
 func TestReadIndex(t *testing.T) {
-	reader := IndexReader("testdata/tenXbarcodeTest.bam.bai")
-	for i, err := simpleio.ReadLine(reader); !err; i, err = simpleio.ReadLine(reader) {
-		fmt.Printf("%s\n", i.String())
-	}
-
-}
-
-func toInt(t *testing.T, s string) int {
-	i, err := strconv.Atoi(s)
-	assert.Nil(t, err)
-	return i
-}
-
-func writeBin(t *testing.T, w io.Writer, s string) {
-	bins := strings.Split(s, ":")
-	// Write the number of bins
-	err := binary.Write(w, binary.LittleEndian, int32(len(bins)))
-	expect.Nil(t, err)
-
-	for _, bin := range bins {
-		binContent := strings.Split(bin, ",")
-
-		// Write the bin number
-		err = binary.Write(w, binary.LittleEndian, assert.Nil(t, uint32(simpleio.StringToInt(binContent[0]))))
-		expect.Nil(t, err)
-		binContent = binContent[1:]
-
-		// Write the number of chunks
-		err = binary.Write(w, binary.LittleEndian, int32(len(binContent)/2))
-		expect.Nil(t, err)
-
-		// Write the chunks
-		for _, voffset := range binContent {
-			err = binary.Write(w, binary.LittleEndian, assert.Nil(t, uint32(simpleio.StringToInt(voffset))))
-			expect.Nil(t, err)
-		}
-	}
-}
-
-func writeIntervals(t *testing.T, w io.Writer, s string) {
-	intervals := strings.Split(s, ",")
-
-	// Write the number of intervals
-	err := binary.Write(w, binary.LittleEndian, int32(len(intervals)))
-	expect.Nil(t, err)
-
-	for _, voffset := range intervals {
-		err = binary.Write(w, binary.LittleEndian, assert.Nil(t, uint64(simpleio.StringToInt(voffset))))
-		expect.Nil(t, err)
-	}
-}
-
-func writeIndex(t *testing.T, bins, intervals []string, unmapped int) *bytes.Buffer {
-	var buf bytes.Buffer
-	magic := []byte{'B', 'A', 'I', 0x1}
-	_, err := buf.Write(magic)
-	expect.Nil(t, err)
-
-	// Two references
-	err = binary.Write(&buf, binary.LittleEndian, int32(len(bins)))
-	expect.Nil(t, err)
-
-	for i := range bins {
-		writeBin(t, &buf, bins[i])
-		writeIntervals(t, &buf, intervals[i])
-	}
-
-	// Write unmapped count
-	if unmapped >= 0 {
-		err = binary.Write(&buf, binary.LittleEndian, uint64(unmapped))
-		expect.Nil(t, err)
-	}
-
-	return &buf
+	idx := IndexReader("testdata/tenXbarcodeTest.bam.bai")
+	t.Logf("%v\n", idx.Refs)
 }
