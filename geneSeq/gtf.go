4742cac4b65fcfd84afe91871020de3c76b3693a
edotau <ericdotau@gmail.com>
2021-05-05 04:47:08 -0700

    gtf reader works! next is to optimize

--- geneSeq/gtf.go
+++ geneSeq/gtf.go
@@ -2,9 +2,9 @@ package geneSeq
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"runtime"
-	"strconv"
 	"strings"
 
 	"github.com/edotau/goFish/simpleio"
@@ -14,7 +14,6 @@ import (
 // Version is the GTF version
 const Version = 2.2
 
-// Feature is the gff feature struct
 type Gtf struct {
 	SeqName    string
 	Source     string
@@ -46,115 +45,9 @@ func ReadFilteredFeatures(file string, chrs []string, feats []string, attrs []st
 	if _, err := os.Stat(file); os.IsNotExist(err) {
 		return nil, err
 	}
-	chrsMap := make(map[string]struct{}, len(chrs))
-	for _, chr := range chrs {
-		chrsMap[strings.ToLower(chr)] = struct{}{}
-	}
-
-	featsMap := make(map[string]struct{}, len(feats))
-	for _, f := range feats {
-		featsMap[strings.ToLower(f)] = struct{}{}
-	}
-
-	attrsMap := make(map[string]struct{}, len(attrs))
-	for _, f := range attrs {
-		attrsMap[strings.ToLower(f)] = struct{}{}
-	}
 
 	fn := func(line string) (interface{}, bool, error) {
-		if len(line) == 0 || line[0] == '#' {
-			return nil, false, nil
-		}
-		line = strings.TrimRight(line, "\r\n")
-		items := strings.Split(line, "\t")
-
-		if len(items) != 9 {
-			return nil, false, nil
-		}
-
-		if len(chrs) > 0 { // selected chrs
-			if _, ok := chrsMap[strings.ToLower(items[0])]; !ok {
-				return nil, false, nil
-			}
-		}
-
-		if len(feats) > 0 { // selected features
-			if _, ok := featsMap[strings.ToLower(items[2])]; !ok {
-				return nil, false, nil
-			}
-		}
-		var err error
-
-		start, err := strconv.Atoi(items[3])
-		if err != nil {
-			return nil, false, fmt.Errorf("%s: bad start: %s", items[0], items[3])
-		}
-
-		end, err := strconv.Atoi(items[4])
-		if err != nil {
-			return nil, false, fmt.Errorf("%s: bad end: %s", items[0], items[4])
-		}
-
-		if start > end {
-			return nil, false, fmt.Errorf("%s: start (%d) must be < end (%d)", items[0], start, end)
-		}
-
-		var score *float64
-		if items[5] != "." {
-			s, err := strconv.ParseFloat(items[5], 64)
-			if err != nil {
-				return nil, false, fmt.Errorf("%s: bad score: %s", items[0], items[5])
-			}
-			score = &s
-		}
-
-		var strand *byte
-		if items[6][0] != '.' {
-			s := items[6][0]
-			if !(s == '+' || s == '-') {
-				return nil, false, fmt.Errorf("%s: illigal strand: %v", items[0], s)
-			}
-
-			strand = &s
-		}
-
-		var frame *int
-		if items[7] != "." {
-			f, err := strconv.Atoi(items[7])
-			if err != nil {
-				return nil, false, fmt.Errorf("%s: bad frame: %s", items[0], items[7])
-			}
-			if !(f == 0 || f == 1 || f == 2) {
-				return nil, false, fmt.Errorf("%s: illigal frame: %d", items[0], f)
-			}
-			frame = &f
-		}
-
-		feature := Gtf{items[0], items[1], items[2], start, end, score, strand, frame, nil}
-
-		tagValues := strings.Split(items[8], "; ")
-		if len(tagValues) > 0 {
-			var ok bool
-			feature.Attributes = []Attribute{}
-			for _, tagValue := range tagValues[0 : len(tagValues)-1] {
-				items2 := strings.SplitN(tagValue, " ", 2)
-				tag := items2[0]
-				if _, ok = attrsMap[tag]; !ok {
-					continue
-				}
-				value := items2[1]
-				// if value[len(value)-1] == ';' {
-				// 	value = value[0 : len(value)-1]
-				// }
-				if len(value) > 2 {
-					value = value[1 : len(value)-1]
-				} else {
-					value = ""
-				}
-				feature.Attributes = append(feature.Attributes, Attribute{tag, value})
-			}
-		}
-		return feature, true, nil
+		return GetGtfLine(line)
 	}
 	reader, err := breader.NewBufferedReader(file, Threads, 100, fn)
 	if err != nil {
@@ -170,6 +63,70 @@ func ReadFilteredFeatures(file string, chrs []string, feats []string, attrs []st
 		}
 	}
 	return features, nil
+}
+
+func GetGtfLine(line string) (interface{}, bool, error) {
+	line = strings.TrimRight(line, "\r\n")
+	items := strings.Split(line, "\t")
+	if len(line) == 0 || line[0] == '#' {
+		return nil, false, nil
+	}
+	start := simpleio.StringToInt(items[3])
+
+	end := simpleio.StringToInt(items[4])
+
+	if start > end {
+		return nil, false, fmt.Errorf("%s: start (%d) must be < end (%d)", items[0], start, end)
+	}
+
+	var score *float64
+
+	if items[5] != "." {
+		s := simpleio.StringToFloat64(items[5])
+		score = &s
+	}
+
+	var strand *byte
+	if items[6][0] != '.' {
+		s := items[6][0]
+		if !(s == '+' || s == '-') {
+			log.Fatalf(fmt.Sprintf("%s: illigal strand: %v", items[0], s))
+		}
+		strand = &s
+	}
+	var frame *int
+	if items[7] != "." {
+		f := simpleio.StringToInt(items[7])
+
+		log.Fatalf(fmt.Sprintf("%s: bad frame: %s", items[0], items[7]))
+
+		if !(f == 0 || f == 1 || f == 2) {
+			simpleio.FatalErr(fmt.Errorf("%s: illigal frame: %d", items[0], f))
+		}
+		frame = &f
+	}
+
+	feature := Gtf{items[0], items[1], items[2], start, end, score, strand, frame, nil}
+
+	tagValues := strings.Split(items[8], "; ")
+	if len(tagValues) > 0 {
+
+		feature.Attributes = []Attribute{}
+		for _, tagValue := range tagValues[0 : len(tagValues)-1] {
+			items2 := strings.SplitN(tagValue, " ", 2)
+			tag := items2[0]
+
+			value := items2[1]
+
+			if len(value) > 2 {
+				value = value[1 : len(value)-1]
+			} else {
+				value = ""
+			}
+			feature.Attributes = append(feature.Attributes, Attribute{tag, value})
+		}
+	}
+	return feature, true, nil
 }
 
 /*
@@ -279,19 +236,18 @@ func (gf *Gtf) ToString() string {
 	str.WriteByte('\t')
 	if gf.Score == nil {
 		str.WriteByte('.')
-		str.WriteByte('\t')
 	} else {
 		str.WriteString(simpleio.Float64ToString(*gf.Score))
 	}
+	str.WriteByte('\t')
 	if gf.Strand == nil {
 		str.WriteByte('.')
-		str.WriteByte('\t')
 	} else {
 		str.WriteByte(*gf.Strand)
 	}
+	str.WriteByte('\t')
 	if gf.Frame == nil {
 		str.WriteByte('.')
-		str.WriteByte('\t')
 	} else {
 		str.WriteString(simpleio.IntToString(*gf.Frame))
 	}
--- geneSeq/gtf_test.go
+++ geneSeq/gtf_test.go
@@ -8,7 +8,7 @@ import (
 func TestGtfReading(t *testing.T) {
 	gf, _ := ParseGtfLine("testdata/denovo.reference.transcrtipt.assembly.annotated.gtf")
 	for i := 0; i < 10; i++ {
-		log.Printf("%v\n", gf[i])
+		log.Printf("%v\n", (gf[i].ToString()))
 	}
 
 }
