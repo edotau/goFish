94b38acdaf2bac7888c8cc428feecf94b38464f3
edotau <ericdotau@gmail.com>
2021-05-06 18:22:35 -0700

    random fastq

--- main/randomFq/randomFq.go
+++ main/randomFq/randomFq.go
@@ -5,13 +5,11 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/edotau/goFish/code"
+	"github.com/edotau/goFish/fasta"
 	"github.com/edotau/goFish/fastq"
 	"github.com/edotau/goFish/simpleio"
-	"github.com/edotau/goFish/stats"
-	//"github.com/edotau/goFish/fasta"
-	//"github.com/edotau/goFish/reference/stickleback"
-	//"github.com/edotau/goFish/simpleio"
+
+	"github.com/edotau/goFish/reference/stickleback"
 )
 
 // TODO: program is not finished
@@ -30,7 +28,7 @@ func main() {
 	var expectedNumArgs int = 1
 	flag.Usage = usage
 	log.SetFlags(log.Ldate | log.Ltime)
-	//var ref *string = flag.String("fasta", stickleback.RabsFasta, "fastq reads are generated from an input fasta reference. If you omit this field, the marine stickleback genome will be used")
+	var ref *string = flag.String("fasta", stickleback.RabsFasta, "fastq reads are generated from an input fasta reference. If you omit this field, the marine stickleback genome will be used")
 	var readLength *int = flag.Int("length", 150, "specify a read length.")
 	var numberOfReads *int = flag.Int("number", 10000, "specify how many reads to generate")
 	//var prefix *string = flag.String("prefix", "simulated_reads", "provide a prefix for file names")
@@ -39,30 +37,34 @@ func main() {
 		flag.Usage()
 		log.Fatalf("Error: expecting %d arguments, but got %d\n\n", expectedNumArgs, len(flag.Args()))
 	} else {
-		randomRead := simpleio.NewWriter(fmt.Sprintf("%s_R1.fastq.gz", flag.Arg(0)))
-		for i := 0; i < *numberOfReads; i++ {
-			fq := fastq.NewFastq(*readLength)
-			fq.Name = fmt.Sprintf("%s_GOFISH_%c_R:1", flag.Arg(0), '+')
-			for i := 0; i < len(fq.Seq); i++ {
-				fq.Seq[i] = code.NoMaskDnaArray[stats.RandIntInRange(0, len(code.NoMaskDnaArray))]
+		genome := fasta.Read(*ref)
+		readOne := simpleio.NewWriter(fmt.Sprintf("%s_R1.fastq.gz", flag.Arg(0)))
+		defer readOne.Close()
 
-			}
-			randomRead.Write(fastq.ToBytes(&fq))
+		readTwo := simpleio.NewWriter(fmt.Sprintf("%s_R2.fastq.gz", flag.Arg(0)))
+		defer readTwo.Close()
+		fqs := fastq.RandomPairedReads(genome, *readLength, *numberOfReads, 500)
+		for _, i := range fqs {
+			readOne.Write(fastq.ToBytes(&i.ReadOne))
+			readTwo.Write(fastq.ToBytes(&i.ReadTwo))
 		}
-		randomRead.Close()
-		/*
-			genome := fasta.Read(*ref)
-			readOne := simpleio.NewWriter(fmt.Sprintf("%s_R1.fastq.gz", flag.Arg(0)))
-			defer readOne.Close()
 
-			readTwo := simpleio.NewWriter(fmt.Sprintf("%s_R2.fastq.gz", flag.Arg(0)))
-			defer readTwo.Close()
-			fqs := fastq.RandomPairedReads(genome, *readLength, *numberOfReads, 500)
-			for _, i := range fqs {
-				readOne.Write(fastq.ToBytes(&i.ReadOne))
-				readTwo.Write(fastq.ToBytes(&i.ReadTwo))
+		/*
+			randomRead := simpleio.NewWriter(fmt.Sprintf("%s_R1.fastq.gz", flag.Arg(0)))
+			for i := 0; i < *numberOfReads; i++ {
+				fq := fastq.NewFastq(*readLength)
+				fq.Name = fmt.Sprintf("%s_GOFISH_%c_R:1", flag.Arg(0), '+')
+				for i := 0; i < len(fq.Seq); i++ {
+					fq.Seq[i] = code.NoMaskDnaArray[stats.RandIntInRange(0, len(code.NoMaskDnaArray))]
+
+				}
+				randomRead.Write(fastq.ToBytes(&fq))
 			}
+			randomRead.Close()
 		*/
+		/*
+
+		 */
 	}
 
 }
