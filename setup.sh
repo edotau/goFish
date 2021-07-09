a05fd5dabaa1ba2a2c521da54cf53c22fc87ec7c
Eric Au <ericdotau@gmail.com>
2021-07-08 19:27:06 -0700

    fixed bugs in setup.sh script

--- setup.sh
+++ setup.sh
@@ -69,27 +69,23 @@ URL=$SOURCE/$FILENAME
 wget $URL
 tar xf $FILENAME -C $HOME; rm $FILENAME
 
-if ! [ -e "/usr/local/go" ] ; then
-    mv go/ /usr/local/
-fi
-
 # Sets up bash profile for go root, path, and bin
 # Note that it is assumed your GOPATH will be placed in your home directory
 if [ "$SHELL" == "/bin/bash" ]; then
-    echo 'export GOROOT=/usr/local/go' >> ~/.bashrc
+    echo 'export GOROOT=~/go' >> ~/.bashrc
     echo 'export GOPATH=$HOME' >> ~/.bashrc
     echo 'export GOBIN=${GOPATH}/bin' >> ~/.bashrc
     echo 'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >> ~/.bashrc
     source ~/.bashrc
 elif [ "$SHELL" == "*zsh*" ]; then
 # Sets up zsh profile for go root, path, and bin if your default shell is zsh
-    echo 'export GOROOT=/usr/local/go' >> ~/.zshrc
+    echo 'export GOROOT=~/go' >> ~/.zshrc
     echo 'export GOPATH=$HOME' >> ~/.zshrc
     echo 'export GOBIN=${GOPATH}/bin' >> ~/.zshrc
     echo 'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >> ~/.zshrc
     source ~/.zshrc
 else
-    echo 'export GOROOT=/usr/local/go' >> ~/.env
+    echo 'export GOROOT=~/go' >> ~/.env
     echo 'export GOPATH=$HOME' >> ~/.env
     echo 'export GOBIN=${GOPATH}/bin' >> ~/.env
     echo 'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >> ~/.env
