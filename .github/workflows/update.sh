112a5618608baa9327f341d24b1f4ca6dbbb9a35
Eric Au <ericdotau@gmail.com>
2022-01-23 23:53:05 -0800

    Dockerfile to debug in a container environment

---
+++ .github/workflows/update.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+set -eou pipefail
+
+release_list=https://golang.org/dl/
+SOURCE=https://storage.googleapis.com/golang
+# TODO Refine logic for setting up GOPATH in other locations
+export DIR=/usr/local
+
+# Figures out what OS you are using
+os=$(uname -s | tr "[:upper:]" "[:lower:]")
+
+# Determines the arch distribution of your current machine using switch cases
+arch_probe="uname -m"
+arch=$($arch_probe)
+case "$arch" in
+    i*)
+        arch=386
+        ;;
+    x*)
+        arch=amd64
+        ;;
+    aarch64)
+        #arch=armv6l
+        arch=arm64
+        ;;
+    armv7l)
+        # Go project does not provide a binary release for armv71
+        echo 'armv7l is not supported, using armv6l'
+        arch=armv6l
+        ;;
+esac
+
+# Performs a quick query for golang releases, excluding beta distributions
+queryReleaseList() {
+    local fetch="$*"
+    if hash "jq" 2>/dev/null; then
+        local rl="$release_list?mode=json"
+        $fetch "$rl" | jq -r '.[].files[].version' | sort | uniq | grep -v -E 'go[0-9\.]+(beta|rc)'| sed -e 's/go//' | sort -V
+    else
+        $fetch "$release_list" | grep -v -E 'go[0-9\.]+(beta|rc)' | grep -E -o 'go[0-9\.]+' | grep -E -o '[0-9]\.[0-9]+(\.[0-9]+)?' | sort -V | uniq
+    fi
+}
+# From the query to fetch all golang releases, we pull the latest stable release
+fetchUpdate() {
+    local last=
+    local fetch=
+    if hash "wget" 2>/dev/null; then
+	    fetch="wget -qO-"
+    elif hash "curl" 2>/dev/null; then
+	    fetch="curl --silent"
+    else
+        echo "Error: local wget and/or curl is not functional..."
+        exit 3
+    fi
+    last=$(queryReleaseList "$fetch" | tail -1)
+    if echo "$last" | grep -q -E '[0-9]\.[0-9]+(\.[0-9]+)?'; then
+	    release=$last
+    fi
+}
+
+# Performs the function call to pull the latest stable release
+fetchUpdate
+
+# Set our variables
+label=go$release.$os-$arch
+FILENAME=$label.tar.gz
+URL=$SOURCE/$FILENAME
+
+# Updating requires sudo:
+sudo rm -rf ${DIR}/go
+
+# Download and decompress golang stable release
+wget $URL
+tar xf $FILENAME -C $DIR; rm $FILENAME
---
+++ Dockerfile
@@ -0,0 +1,45 @@
+FROM ubuntu:latest
+
+ENV TZ=America/New_York
+RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+
+RUN apt-get update && apt-get install --yes --no-install-recommends \
+  curl \
+  wget \
+  git \
+  vim \
+  cmake \
+  python3 \
+  libssl-dev \
+  libcurl4-openssl-dev \
+  libxml2-dev \
+  libpng-dev \
+  build-essential \
+  nfs-kernel-server \
+  && apt-get autoclean \
+  && curl -sL https://deb.nodesource.com/setup_14.x  | bash - \
+  && apt-get install -y python-setuptools \
+  default-jre \
+  && apt-get install -y alien unzip wget libz-dev \
+  && apt-get clean \
+  && rm -rf /var/lib/apt/lists/*
+
+RUN curl https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh \
+  > Miniconda3-latest-Linux-x86_64.sh \
+  && yes \
+  | bash Miniconda3-latest-Linux-x86_64.sh -b
+RUN rm Miniconda3-latest-Linux-x86_64.sh
+
+# put system path first so that conda doesn't override python
+ENV PATH=$PATH:/root/miniconda3/bin/
+
+# install "report" environment's dependencies
+RUN conda update -n base -c defaults conda
+COPY environment.yml /
+RUN conda env create -f environment.yml
+
+# install "report" environment's dependencies
+RUN conda update -n base -c defaults conda
+COPY environment.yml /
+RUN conda env create -f environment.yml
+ENV PATH=/root/miniconda3/envs/octantbio/bin/:$PATH
