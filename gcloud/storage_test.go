eeb8ca0b7015290b72c73e3c4c4d484bbb335f9c
Eric Au <eau@eau-mbp.domain>
2022-03-30 14:55:18 -0700

    comment out gcp code because I nolong have access

--- gcloud/storage_test.go
+++ gcloud/storage_test.go
@@ -1,11 +1,11 @@
 package gcloud
 
-import (
-	"testing"
-)
+// import (
+// 	"testing"
+// )
 
-func TestGcloudAuth(t *testing.T) {
-	//GetAllBuckets("../../../.apikey/bcr-storage-key.json", "bcr-3-264801")
-	//DownloadFile("bli-bcr-dev-abe-standard", "betafold_20220301_000_TEST/1A0Q/1A0Q_ranked_0.pdb", "testdata/1A0Q_ranked_0.pdb")
-	DownloadPdb("bli-bcr-dev-abe-standard", "betafold_2022030_TEST", "testdata")
-}
+// func TestGcloudAuth(t *testing.T) {
+// 	//GetAllBuckets("../../../.apikey/bcr-storage-key.json", "bcr-3-264801")
+// 	//DownloadFile("bli-bcr-dev-abe-standard", "betafold_20220301_000_TEST/1A0Q/1A0Q_ranked_0.pdb", "testdata/1A0Q_ranked_0.pdb")
+// 	DownloadPdb("bli-bcr-dev-abe-standard", "betafold_2022030_TEST", "testdata")
+// }
